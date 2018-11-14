dcac0d4781af000b9959cccd73d628a779a070b1
Brantlee Mercer <brantleemercer@gmail.com>
2018-11-14 02:38:41 -0500

    Spent a little time and made it look better.  Also randomly generates a
    percentage of the board as walls.

--- FlightPath/src/flightPath/Cell.java
+++ FlightPath/src/flightPath/Cell.java
@@ -1,10 +1,36 @@
 package flightPath;
 
+import java.awt.Color;
+import java.awt.Dimension;
 
-
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
-public class Cell  extends JButton{
+public class Cell extends JButton {
+	private boolean isWall=false;
+	private int location;
+	private Color wall = Color.black;
+	private static int width = 50;
+	private static int height = 50;
+	private static Color backgroundColor = Color.white;
+
+	private void changeColor(Color c) {
+		// using JButton code to set the change color method
+		setBackground(c);
+		paintImmediately(0, 0, width, height);}
+		
+	//Constructor for the cells
+	public Cell(int location) {
+		//To use to find where node is on layout
+		this.location = location;
+		setPreferredSize(new Dimension(width, height));
+		setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+		changeColor(backgroundColor);
+		
+	}
 	
-	
+	//used to draw the cells black that will be a wall
+	public void drawWall() {
+		changeColor(wall);
+	}
 }
--- FlightPath/src/flightPath/Layout.java
+++ FlightPath/src/flightPath/Layout.java
@@ -2,14 +2,22 @@ package flightPath;
 
 import java.awt.Container;
 import java.awt.GridLayout;
-
+import java.lang.Math;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 @SuppressWarnings("serial")
 public class Layout extends JFrame {
-	private Cell[] cellBoard = new Cell[81];
+
+	// this will be variable by the end, 10 is just a test number
+	int numOfNodes = 25;
+	boolean foundWall;
+	private Cell[] cellBoard = new Cell[(int) Math.pow(numOfNodes, 2)];
+	ArrayList<Integer> masterNodeLocations = new ArrayList<>();
+	ArrayList<Integer> wallList = new ArrayList<>();
 
 	public static void main(String[] args) {
 
@@ -19,28 +27,52 @@ public class Layout extends JFrame {
 	private JPanel p = new JPanel();
 
 	public Layout() {
-		setTitle("Layout");
+		setTitle("Flight Layout");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(700, 700);
 		setResizable(false);
 
 		Container c = getContentPane();
-		p.setLayout(new GridLayout(9, 9));
 
-		// Chooses the color to make each cell
+		// Adds numbers to a list to be used to determine walls
+		for (int x = 0; x < (int) (Math.pow(numOfNodes, 2)); x++) {
+			masterNodeLocations.add(x);
+		}
 
-		for (int x = 0; x < 81; x++) {
-			Cell nextCell;
+		p.setLayout(new GridLayout(numOfNodes, numOfNodes));
+		masterWallList();
+		
 
-			// Creates the new cell after finally figuring out the background color
-			nextCell = new Cell();
+		for (int x = 0; x < (int) Math.pow(numOfNodes, 2); x++) {
+			Cell nextCell = new Cell(x);
+
+			if (wallList.contains(x)) {
+				nextCell.drawWall();
+			}
+			
 
 			cellBoard[x] = nextCell;
 			p.add(cellBoard[x]);
-			// adds the cell to the game board itself
-			c.add(p);
 
-			setVisible(true);
+			// adds the cell to the flight layout
+			c.add(p);
 		}
+		setVisible(true);
+	}
+
+	// Used to create a list of locations that will be walls
+	private void masterWallList() {
+		Random rand = new Random();
+		int percentage = (int) (masterNodeLocations.size() * (float) .25);
+		ArrayList<Integer> temp = masterNodeLocations;
+
+		for (int x = 0; x < percentage; x++) {
+			int wallNum = rand.nextInt(temp.size());
+			while (wallList.contains(wallNum) || wallNum == 0 || wallNum == temp.size()-1) {
+				wallNum = rand.nextInt(temp.size());
+			}
+			wallList.add(wallNum);
+		}
+		Collections.sort(wallList);
 	}
 }
