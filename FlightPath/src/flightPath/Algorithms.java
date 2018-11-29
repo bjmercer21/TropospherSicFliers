f8190acae77261f24f17b1d3455b19165da0d326
Brantlee Mercer <brantleemercer@gmail.com>
2018-11-29 15:46:44 -0500

    ..

--- FlightPath/src/flightPath/Algorithms.java
+++ FlightPath/src/flightPath/Algorithms.java
@@ -3,14 +3,16 @@ package flightPath;
 
 public class Algorithms {
 
-	static int testNum = 25;
+	static int testNum = 50;
 
 	public static Layout board = new Layout(testNum,testNum);
 
 
 
 	public static void main(String[] args) {
-		new AStar(board);		
+		//new AStar(board);	
+		
+		new DepthFirst(board);
 	}
 
 
