51ea2a66f8124ac38ca88ef8fb355d5ba44310c6
Gary <grr4558@uncw.edu>
2018-12-01 23:09:55 -0500

    DepthFirst algorithm, same as DFS with slightly difference of being able to backtrack

--- FlightPath/src/flightPath/Algorithms.java
+++ FlightPath/src/flightPath/Algorithms.java
@@ -3,14 +3,15 @@ package flightPath;
 
 public class Algorithms {
 
-	static int testNum = 50;
+	static int testNum = 250;
 
 	public static Layout board = new Layout(testNum,testNum);
 
 
 
 	public static void main(String[] args) {
-		//new AStar(board);	
+		//new AStar(board);
+                new BFS(board);
 		
 		new DepthFirst(board);
 	}
